b5ad328b3ea066752b05c6cac59b24ca000e7494
barry <barryhamzata@gmail.com>
2025-02-22 18:29:47 +0100

    Control

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)        
         screen.fill("black")
-        player.draw(screen)
-        
+        for item in drawable:
+            item.draw(screen)
+
         dt = clock.tick(60)/1000
         pygame.display.flip()
 
--- player.py
+++ player.py
@@ -15,3 +15,20 @@ class Player (CircleShape):
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen,"white",points=self.triangle(),width=2)
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED*dt
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_q]:
+            self.rotate(dt*-1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(dt*-1)
+        if keys[pygame.K_z]:
+            self.move(dt)
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
