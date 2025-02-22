90dfa4e51ee5b4f182f77a895c4f6617a5af35d6
barry <barryhamzata@gmail.com>
2025-02-22 14:25:37 +0100

    print

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
-from constants import connect_database
+from constants import *
 
 def main():
     print ("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
